cb259557b222f66c71eb5b45c96d1eb2099f8239
Piyush Prajapati <piyushoa2004@gmail.com>
2023-11-25 16:05:48 +0530

    25-nov-2

--- controller/authentication.js
+++ controller/authentication.js
@@ -3,59 +3,37 @@ import User from "../schema/User.js";
 // signup
 export const signup = async (req, res, next) => {
     const { name, email, password, confirmPassword } = req.body;
-    // let isError = false;
-    // const errorObject = {};
 
     let result;
 
     try {
 
         if (name.trim() === "" || email.trim() === "" || password.trim() === "" || confirmPassword === "") {
-            // isError = true;
-            // errorObject["EMPTY_FIELD"] = "Fields can't be empty.";
             throw { message: "Fields can't be empty." }
         }
         if (email.indexOf("@") === -1) {
-            // isError = true;
-            // errorObject["INVALID_EMAIL"] = "Invalid email address.";
             throw { message: "Invalid email address." }
         }
         if (password.length < 5) {
-            // isError = true;
-            // errorObject["WEAK_PASSWORD"] = "Weak password. Please use a strong password."
             throw { message: "Weak password. Please use a strong password." }
         }
         if (password !== confirmPassword) {
-            // isError = true;
-            // errorObject["PASSWORD_MISMATCH"] = "Password mismatch.";
             throw { message: "Password mismatch." }
         }
 
         result = await User.findOne({ email: email });
 
         if (result !== null) {
-            // isError = true;
-            // errorObject["USER_ALREADY_EXIST"] = "User already exist. Login to continue."
             throw { message: "User already exist. Login to continue." }
         }
-        // if (isError) {
-        //     throw {message: errorObject}
-        // }
 
         result = await User.create({ name: name, email: email, password: password });
-        res.send("Account created successfully");
+        res.send("Account created successfully. Login to continue.");
 
     } catch (error) {
         res.status(422).send(error.message);
     }
-
-    // res.send(result);
 }
-
-// const sanitizedPayload = (payload) => {
-
-
-// }
 
 // login
 export const login = async (req, res, next) => {
@@ -68,10 +46,49 @@ export const login = async (req, res, next) => {
             throw { message: "Fields can't be empty." }
         }
         result = await User.findOne({ email: email, password: password });
+
         if (result === null) {
             throw { message: "Invalid Credentials. Please try again." }
         }
-        res.send("Login successfull");
+        res.send("Login successfull.");
+    } catch (error) {
+        res.status(422).send(error.message);
+    }
+}
+
+// reset password
+
+export const resetpassword = async (req, res, next) => {
+    const { email, otp, password, confirmPassword } = req.body;
+
+    let result;
+
+    try {
+        if (email.trim() === "") {
+            throw { message: "Email can't be empty." }
+        }
+
+        result = await User.findOne({ email: email });
+
+        if (email.indexOf("@") === -1 || result === null) {
+            throw { message: "Invalid email address" }
+        }
+        if (otp.trim() === "" || otp !== "123") {
+            throw { message: "Incorrect OTP. Please try again." }
+        }
+        if (password.trim() === "" || confirmPassword.trim() === "") {
+            throw { message: "Password can't be empty" }
+        }
+        if (password.length < 5) {
+            throw { message: "Weak password. Please choose a strong password." }
+        }
+        if (password !== confirmPassword) {
+            throw { message: "Password mismatch." }
+        }
+
+        await User.updateOne({ email: email }, { password: password });
+        res.send("Password reset successfull. Login to continue");
+
     } catch (error) {
         res.status(422).send(error.message);
     }
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { signup, login } from "../controller/authentication.js";
+import { signup, login, resetpassword } from "../controller/authentication.js";
 
 const router = Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/resetpassword", resetpassword);
 
 export default router;
